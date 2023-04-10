c23ecddf4eb1fce1e7b36f8e2e25427941d9a696
Vitalino Silva <vitalinosilva@gmail.com>
2023-04-10 18:16:39 +0100

    initial commit

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,6 +1,15 @@
 import { useTranslation } from 'react-i18next';
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {getAllTasks} from "../store/tasks-store-slice";
+import {AppDispatch} from "../store";
 export function HomePage(): JSX.Element {
+    const dispatch = useDispatch<AppDispatch>();
     const { t } = useTranslation();
+
+    useEffect(() => {
+        dispatch(getAllTasks());
+    }, []);
 
 
     return (<div>
