261dff7cc4240c071b9ae69a44e71179e72fe119
Vitalino Silva <vitalinosilva@gmail.com>
2023-04-14 14:31:15 +0100

    created the title component

---
+++ src/components/title-component/title-component-props.ts
@@ -0,0 +1,3 @@
+export interface TitleComponentProps {
+    title?: string;
+}
---
+++ src/components/title-component/title-component.tsx
@@ -0,0 +1,15 @@
+import type {TitleComponentProps} from "./title-component-props";
+
+export function TitleComponent(props: TitleComponentProps): JSX.Element {
+
+    const {title} = props;
+
+    return (<div className='title-component__container' data-testid='title-component__container'>
+        <h1 className='title-component__title' data-testid='title-component__title'
+        >{
+            !title
+                ? (<span className='title-component__title__text' data-testid='title-component__title__text'></span>)
+                : (<span className='title-component__title__text' data-testid='title-component__title__text'>{title}</span>)
+        }</h1>
+    </div>);
+}
