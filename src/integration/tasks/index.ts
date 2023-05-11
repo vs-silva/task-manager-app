4b5d1725bf9fbb0102f0abc6038453c1260f64a6
Vitalino Silva <vitalinosilva@gmail.com>
2023-05-11 11:41:17 +0100

    removed objects from mocked-data

--- src/integration/tasks/index.ts
+++ src/integration/tasks/index.ts
@@ -1,7 +1,8 @@
 import {TasksService} from "./tasks.service";
-//import {MockDataReaderAdapter} from "./adapters/mock-data-reader.adapter";
-//import {MockDataWriterAdapter} from "./adapters/mock-data-writer.adapter";
+import {MockDataReaderAdapter} from "./adapters/mock-data-reader.adapter";
+import {MockDataWriterAdapter} from "./adapters/mock-data-writer.adapter";
 import {RestApiWriterAdapter} from "./adapters/restapi-writer.adapter";
 import {RestApiReaderAdapter} from "./adapters/restapi-reader.adapter";
 
-export default TasksService(RestApiReaderAdapter(), RestApiWriterAdapter());
+export default TasksService(MockDataReaderAdapter(), MockDataWriterAdapter());
+//export default TasksService(RestApiReaderAdapter(), RestApiWriterAdapter());
